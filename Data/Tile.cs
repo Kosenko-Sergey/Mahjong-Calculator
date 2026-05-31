3c0683aa6efe877ab80939a0ba55454cb1729808
SergeyKosenko <winget2002@gmail.com>
2026-05-31 15:04:12 +0700

    Added tile assets

---
+++ Data/Tile.cs
@@ -0,0 +1,13 @@
+﻿namespace Mahjong_Calculator.Data
+{
+    public class Tile
+    {
+        public string Name { get; set; }
+        public int VisualType { get; set; }
+        public string TileType { get; set; }
+        public int Value { get; set; }
+        public string TileBG { get; set; }  
+        public string TileFG { get; set; } 
+        public int ZIndex { get; set; }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mahjong_Calculator;
 
-public partial class MainPage : ContentPage
+public partial class MainPage : TabbedPage
 {
 	public MainPage()
 	{
